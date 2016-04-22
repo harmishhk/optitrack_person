b4d7e610c55bb554aa035c2aeb5d19765ecdd16f
harmish khambhaita <harmish@laas.fr>
2016-04-22 17:16:00 +0200

    adding sleep before connecting and removing path in tcl script

--- scripts/connect_optitrack.tcl
+++ scripts/connect_optitrack.tcl
@@ -2,6 +2,6 @@
 
 package require genomix
 genomix::connect
-genomix1 rpath ~/openrobots/devel/lib/genom/ros/plugins/
 genomix1 load optitrack
+after 1000
 optitrack::connect {host marey host_port 1510 mcast 255.255.255.255 mcast_port 1511}
